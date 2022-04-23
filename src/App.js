48b19564aafe0b16f8668e38a7802610afe10f44
walter <meza.walter@gmail.com>
2022-04-23 12:03:47 +1000

    Testing some components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function App(){
+    return(
+        <>
+        <h1>Hello World {5+5}</h1>
+        <h2>testing</h2>
+        <h3>testing 2</h3>
+        <h4>hi</h4>
+        </>
+    )
+}
+
+export default App;
